67c81cf93b301b3bc901a274ee87a415ab207a02
lokiki0319@gamil.com <lokiki0319@gmail.com>
2024-04-30 01:28:51 +0900

    데이터베이스 ERD 변경

--- db/initdb.d/create_table.sql
+++ db/initdb.d/create_table.sql
@@ -1,63 +1,74 @@
-
-use youtube;
-
-create table refresh_token
+create table if not exists youtube.advertisement
 (
-    id            int unsigned auto_increment
-        primary key,
-    user_id       int unsigned not null,
-    refresh_token varchar(255) not null
+    ad_id int unsigned not null
+        primary key
 );
 
-create table users
+create table if not exists youtube.users
 (
     user_id    int unsigned auto_increment
         primary key,
-    username   varchar(10)                        not null,
-    email      varchar(20)                        not null,
-    password   varchar(20)                        not null,
+    username   varchar(30)                        null,
+    email      varchar(50)                        not null,
+    password   varchar(255)                       not null,
     role_type  int                                not null,
     created_at datetime default CURRENT_TIMESTAMP not null,
     constraint email
         unique (email)
 );
 
-create table video
+create table if not exists youtube.refresh_token
+(
+    id            int unsigned auto_increment
+        primary key,
+    user_id       int unsigned not null,
+    refresh_token varchar(255) not null,
+    constraint refresh_token_users_user_id_fk
+        foreign key (user_id) references youtube.users (user_id)
+);
+
+create table if not exists youtube.video
 (
     video_id          int unsigned auto_increment
         primary key,
-    title             varchar(30)                            not null,
-    duration          int                                    not null,
-    content           varchar(50)                            not null,
-    created_at        timestamp    default CURRENT_TIMESTAMP null,
-    updated_at        timestamp    default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
-    author            varchar(20)                            not null,
-    video_total_views int unsigned default '0'               null,
-    video_ad_views    int unsigned default '0'               not null,
+    title             varchar(30)                               not null,
+    duration          int unsigned    default '0'               not null,
+    content           varchar(50)                               not null,
+    created_at        timestamp       default CURRENT_TIMESTAMP null,
+    updated_at        timestamp       default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    author            varchar(50)                               not null,
+    video_total_views bigint unsigned default '0'               null,
+    video_ad_views    bigint unsigned default '0'               null,
     constraint video_ibfk_2
-        foreign key (author) references users (email)
+        foreign key (author) references youtube.users (email)
 );
 
-create table user_history
+create table if not exists youtube.user_history
 (
     play_id         int unsigned auto_increment
         primary key,
-    user_id         int unsigned             null,
-    video_id        int unsigned             null,
-    viewing_time    int unsigned default '0' not null,
-    video_timestamp int unsigned default '0' not null,
+    user_id         int unsigned                 null,
+    video_id        int unsigned                 null,
+    viewing_time    int unsigned default '0'     not null,
+    video_timestamp timestamp    default (now()) not null,
     constraint play_history_ibfk_1
-        foreign key (user_id) references users (user_id),
+        foreign key (user_id) references youtube.users (user_id),
     constraint play_history_ibfk_2
-        foreign key (video_id) references video (video_id)
+        foreign key (video_id) references youtube.video (video_id)
 );
 
-create table video_history
+create table if not exists youtube.video_ad
 (
-    video_history_id int unsigned auto_increment
+    video_ad_id  int unsigned auto_increment
         primary key,
-    video_id         int unsigned                        not null,
-    play_date        timestamp default CURRENT_TIMESTAMP not null,
-    constraint video_history_ibfk_1
-        foreign key (video_id) references video (video_id)
+    video_id     int unsigned                        not null,
+    ad_timestamp timestamp default CURRENT_TIMESTAMP null,
+    ad_id        int unsigned                        not null,
+    constraint video_ad_advertisement_ad_id_fk
+        foreign key (ad_id) references youtube.advertisement (ad_id),
+    constraint video_ad_ibfk_2
+        foreign key (video_id) references youtube.video (video_id)
 );
+
+create index advertisement_detail_ibfk_2
+    on youtube.video_ad (video_id);
